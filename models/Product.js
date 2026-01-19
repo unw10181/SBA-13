ab9b313385d8708183e6111035d6dbe4e0905c37
unw10181 <uchennanwilliams@gmail.com>
2026-01-19 15:30:02 -0500

    schema for products

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema();
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true, min: 0.01 },
+  category: { type: String, required: true },
+  inStock: { type: Boolean, default: true },
+  tags: { type: [String] },
+  createdAt: { type: Date, default: Date.now },
+});
 
-
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
