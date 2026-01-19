7b6ba631dd5737e14d73098b03ad1bebcf5dfecb
unw10181 <uchennanwilliams@gmail.com>
2026-01-19 15:05:00 -0500

    schema base

--- server.js
+++ server.js
@@ -2,3 +2,20 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+// INDUCES
+
+//Port - Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Sever is listening on port: ${PORT}`);
+});
+
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
