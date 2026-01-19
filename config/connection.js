88a2d24cac10699e23c06cee86ca240ae4dfd9a7
unw10181 <uchennanwilliams@gmail.com>
2026-01-19 16:14:36 -0500

    fixed errors

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
-// DB Connection
-mongoose.connect(process.env.DATABASE_URL);
+const dbConnection = async () => {
+  try {
+    // DB Connection
+    mongoose.connect(process.env.DATABASE_URL);
+  } catch (error) {
+    console.error("Error: ", error);
+    process.exit(1);
+  }
+};
 
-// Database Connection Error/Success
-// Define callback functions for various events
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err.message + " is mongo not running?"));
 db.on("connected", () => console.log("mongo connected"));
 db.on("disconnected", () => console.log("mongo disconnected"));
 
-module.exports = mongoose.connection;
+module.exports = dbConnection;
--- server.js
+++ server.js
@@ -2,10 +2,8 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+
 const dbConnection = require("./config/connection");
-
-
-
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -20,5 +18,3 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Sever is listening on port: ${PORT}`);
 });
-
-
