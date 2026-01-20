ba189ad308d70ad0c27580025827f56b78f2db24
unw10181 <uchennanwilliams@gmail.com>
2026-01-19 22:59:12 -0500

    delete and update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,6 @@ const Product = require("../models/Product");
 
 const router = express.Router();
 
-//index
-
-router.get("/products", (req, res) => {
-  res.send("How fa now");
-});
-
 // Create
 router.post("/products", (req, res) => {
   console.log(req.body);
@@ -32,9 +26,48 @@ router.get("/products/:id", async (req, res) => {
     if (!product) {
       return res.status(404).json({ message: "Product not found." });
     }
+
     res.json(product);
   } catch (error) {
     res.status(404).send(error);
+  }
+});
+
+// Put
+router.put("/products/:id", async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    ).exec();
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete, update by ID
+router.delete("/products/:id", async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.redirect("/products");
+  } catch (error) {
+    res.status(501).send(error);
+  }
+});
+
+//index GET ALL
+
+
+router.get("/products", async (req, res) => {
+  try {
+
+  } catch (err) {
+    
   }
 });
 
